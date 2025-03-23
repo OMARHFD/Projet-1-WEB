a68aaa98d23f9e385ed8e5e0f0e7127f4842e533
OMARHFD <omar.hafidi@um6p.ma>
2025-03-23 22:48:08 +0000

    Add files via upload

---
+++ TP23032025(1)/src/views/jobs/JobDetails.vue
@@ -0,0 +1,33 @@
+<template>
+    <div v-if="jobDetail">
+        <h1>Job details {{ id }}</h1>
+      <h3>{{ jobDetail.titre }}</h3>
+      <p>{{ jobDetail.description }}</p>
+    </div>
+    <div v-else >
+        Nothing to see
+    </div>
+      
+    </template>
+    
+    <script>
+    
+    export default {
+        props: ['id'],
+        data() {
+            return {
+                jobDetail : []
+            }
+        },
+        mounted() {
+            fetch("http://localhost:3000/jobs/"+this.id)
+            .then(resp => resp.json())
+            .then(data => this.jobDetail = data)
+            .catch(err => console.log(err))
+        }
+    }
+    </script>
+    
+    <style>
+    
+    </style>
